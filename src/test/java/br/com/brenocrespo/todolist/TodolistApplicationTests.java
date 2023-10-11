d4376be28547b154f030b2964e50f5b4d19ecb09
Breno Crespo <63520916+Breno-Crespo@users.noreply.github.com>
2023-10-11 01:25:14 -0300

    first commit

---
+++ src/test/java/br/com/brenocrespo/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brenocrespo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
